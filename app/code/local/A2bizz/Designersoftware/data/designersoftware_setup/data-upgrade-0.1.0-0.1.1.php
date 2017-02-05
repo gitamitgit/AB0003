65e76ebc4428854a8c5580c284297d615013932b
amitgitamit <amit_game2002@yahoo.co.in>
2017-02-05 14:26:17 +0530

    magento added angle module

---
+++ app/code/local/A2bizz/Designersoftware/data/designersoftware_setup/data-upgrade-0.1.0-0.1.1.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * @category    A2bizz
+ * @package     A2bizz_Designersoftware
+ * @copyright   Copyright (c) 2016-2017  and affiliates (http://www.a2bizz.com)
+ * @license     Open Software License (OSL 3.0)
+ */
+
+$designersoftwareAngles = array(
+    array(
+        'title'         => '000',
+        'filename'    	=> '',
+        'content'       => "",
+        'status'     	=> 1,
+        'created_time'	=> now(),
+        'update_time'	=> now()
+    ),
+    array(
+        'title'         => '045',
+        'filename'    	=> '',
+        'content'       => "",
+        'status'     	=> 1,
+        'created_time'	=> now(),
+        'update_time'	=> now()
+    ),
+    array(
+        'title'         => '090',
+        'filename'    	=> '',
+        'content'       => "",
+        'status'     	=> 1,
+        'created_time'	=> now(),
+        'update_time'	=> now()
+    ),
+    array(
+        'title'         => '135',
+        'filename'    	=> '',
+        'content'       => "",
+        'status'     	=> 1,
+        'created_time'	=> now(),
+        'update_time'	=> now()
+    ),
+    array(
+        'title'         => '180',
+        'filename'    	=> '',
+        'content'       => "",
+        'status'     	=> 1,
+        'created_time'	=> now(),
+        'update_time'	=> now()
+    ),
+    array(
+        'title'         => '225',
+        'filename'    	=> '',
+        'content'       => "",
+        'status'     	=> 1,
+        'created_time'	=> now(),
+        'update_time'	=> now()
+    ),
+    array(
+        'title'         => '270',
+        'filename'    	=> '',
+        'content'       => "",
+        'status'     	=> 1,
+        'created_time'	=> now(),
+        'update_time'	=> now()
+    ),
+    array(
+        'title'         => '315',
+        'filename'    	=> '',
+        'content'       => "",
+        'status'     	=> 1,
+        'created_time'	=> now(),
+        'update_time'	=> now()
+    )
+);
+
+/**
+ * Insert default and system pages
+ */
+foreach ($designersoftwareAngles as $data) {
+    Mage::getModel('designersoftware/angles')->setData($data)->save();
+}
+
