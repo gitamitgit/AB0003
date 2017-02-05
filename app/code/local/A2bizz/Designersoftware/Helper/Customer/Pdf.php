2e7ccd9c732240b5591a45e200dbf31d64138cd8
amitgitamit <amit_game2002@yahoo.co.in>
2017-02-05 13:09:19 +0530

    magento init

---
+++ app/code/local/A2bizz/Designersoftware/Helper/Customer/Pdf.php
@@ -0,0 +1,11 @@
+<?php
+
+class A2bizz_Designersoftware_Helper_Customer_Pdf extends Mage_Core_Helper_Abstract
+{   
+	public function createDirectory($dirName) {
+        if (!is_dir($dirName)) {
+            @mkdir($dirName);
+            @chmod($dirName, 0777);
+        }
+    }
+}
---
+++ app/code/local/A2bizz/Designersoftware/Model/Cart.php
@@ -0,0 +1,6 @@
+<?php
+
+class A2bizz_Designersoftware_Model_Cart extends A2bizz_Designersoftware_Model_Abstract
+{
+    
+}
